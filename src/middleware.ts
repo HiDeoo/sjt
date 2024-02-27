05d5a49c50d81489b8c0a9bd75d4fe79d2d15577
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-02-27 18:45:16 +0100

    chore: add middleware logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,18 @@ import { defineMiddleware } from 'astro:middleware'
 import { parseCommandStr } from './libs/commands'
 
 export const onRequest = defineMiddleware((context, next) => {
+  console.warn('> middleware: onRequest')
+
   const url = new URL(context.url)
+  console.warn('> middleware: url.pathname', url.pathname)
   if (url.pathname !== '/') return next()
 
   const query = url.searchParams.get('q')
+  console.warn('> middleware: query', query)
   if (!query) return next()
 
   const command = parseCommandStr(query)
+  console.warn('> middleware: command.type', command.type)
   if (command.type === 'invalid' || !command.redirect) return next()
 
   return new Response(null, {
